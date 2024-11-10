01ce6c14d41679d809cee20a307c1075f9786bf6
IbnuEyni <105171146+IbnuEyni@users.noreply.github.com>
2024-11-10 20:04:36 +0300

    Update urls.py

--- search_app/urls.py
+++ search_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.query_page, name='query_page'),
-    path('process_query/', views.process_query, name='process_query'),
+    path("", views.query_page, name="query_page"),
+    path("process_query/", views.process_query, name="process_query"),
 ]
